544b990b8a9ce246e0beb3880d07caea56b6bbe5
Belek <belek.salchak.97@mail.ru>
2024-03-29 14:20:40 +0700

    Initial commit

---
+++ src/main/java/crud/app/service/UserService.java
@@ -0,0 +1,14 @@
+package crud.app.service;
+
+import crud.app.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    void save(User user);
+    List<User> findAll();
+    Optional<User> findById(Long id);
+    void updateUser(User user);
+    void deleteById(Long id);
+}
